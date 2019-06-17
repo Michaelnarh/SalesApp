f3b67ceb63fc2cea3ef0511eb0f6b572c7607295
michaelnarh <michaeltetteh62@gmail.com>
2019-06-17 09:37:46 +0000

    ne

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,29 +1,27 @@
-import React, { Component } from 'react'
-import Product from "./Product"
-import Title from "./Title"
-import {StoreProduct} from '../Data'
-import {ProductConsumer} from "../Context"
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { StoreProduct } from "../Data";
+import { ProductConsumer } from "../Context";
 
 export default class ProductList extends Component {
- 
   render() {
     return (
-      <>          
-    <Title me="OUR" u="PRODUCTS"/>
-          
-      <div className="row"> 
-        <div className="text-dark mx-4">
-           <ProductConsumer>  
-             {  (value)=>{
-               return value.product.map(product=>{
-                 return<Product key={product.id} product={product}/>
-               })
-             }}
-           </ProductConsumer>
+      <>
+        <Title me="OUR" u="PRODUCTS" />
+
+        <div className="row">
+          <div className="text-dark mx-4">
+            <ProductConsumer>
+              {value => {
+                return value.product.map(product => {
+                  return <Product key={product.id} product={product} />;
+                });
+              }}
+            </ProductConsumer>
+          </div>
         </div>
-      </div>  
-         
       </>
-    )
+    );
   }
 }
